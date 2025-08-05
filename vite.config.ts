9bd99c904ec2f748205eb879976115db1a3a03bd
Sercan Solmaz <sercan.solmaz@monsternotebook.com>
2025-08-05 10:29:39 +0300

    Update vite configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/monster-stopwatch/',
+  base: '/monsterstopwatch/',
   plugins: [vue()],
   resolve: {
     alias: {
